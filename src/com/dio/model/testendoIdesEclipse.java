c664a22ea4e42d5ef346ecadcc34bd8a682c80ed
freddyvidigal <freddy181208@hotmail.com>
2021-08-16 20:11:18 -0300

    primeiro projeto

---
+++ src/com/dio/model/testendoIdesEclipse.java
@@ -0,0 +1,31 @@
+package com.dio.model;
+
+import com.dio.Gato;
+
+public class testendoIdesEclipse {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+		// TODO Auto-generated method stub
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello world " + (a+b));
+		*/
+		
+		
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
